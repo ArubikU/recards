838a7853436fb3a3c0e991824b1686e8f34835de
Arubik <102335860+ArubikU@users.noreply.github.com>
2025-05-19 19:01:25 -0500

    Update cohere.ts

--- lib/cohere.ts
+++ lib/cohere.ts
@@ -4,21 +4,7 @@ import PDFParser from "pdf2json"
 import { readFile } from "./blob"
 import { importTypes } from "./getLimits"
 
-async function main() {
-  try {
-    // Using the OCR.space default free API key (max 10reqs in 10mins) + remote file
-    const res1 = await ocrSpace('http://dl.a9t9.com/ocrbenchmark/eng.png');
-
-    // Using your personal API key + local file
-    const res2 = await ocrSpace('/path/to/file.pdf', { apiKey: '<API_KEY_HERE>' });
-
-    // Using your personal API key + base64 image + custom language
-    const res3 = await ocrSpace('data:image/png;base64...', { apiKey: '<API_KEY_HERE>', language: 'ita' });
-  } catch (error) {
-    console.error(error);
-  }
-}
-const apiKeys = ["*****"
+const apiKeys = [ "******"
 ]
 
 export function getRandomApiKey() {
@@ -67,7 +53,7 @@ export async function extractCsvText(buffer: Buffer): Promise<string> {
 }
 
 export async function extractImageText(url: string): Promise<string> {
-  const response = await ocrSpace(url, { apiKey: "K83908249788957" })
+  const response = await ocrSpace(url, { apiKey: "*****" })
   return response.ParsedResults[0].ParsedText
 }
 
